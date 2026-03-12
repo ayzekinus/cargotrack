f4539dcdbd8df46f4e07fe2344688cfb9696d32b
ayzekinus <66388861+ayzekinus@users.noreply.github.com>
2026-03-12 14:27:14 +0100

    Update Supabase URL and anon key

--- src/supabase.js
+++ src/supabase.js
@@ -6,7 +6,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";   // ← değiştirin
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";                     // ← değiştirin
+const SUPABASE_URL = "https://tamdwyoulwtufhjhvdnn.supabase.co";   // ← değiştirin
+const SUPABASE_ANON_KEY = "sb_secret_YHBk0h_jIw0XxL2IpaPNfQ_TMn4gZko";                     // ← değiştirin
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
