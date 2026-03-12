4a20c59ce16b63389698a6fd6a7b21d2e591c109
ayzekinus <66388861+ayzekinus@users.noreply.github.com>
2026-03-12 14:32:41 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -7,6 +7,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://tamdwyoulwtufhjhvdnn.supabase.co";   // ← değiştirin
-const SUPABASE_ANON_KEY = "sb_secret_YHBk0h_jIw0XxL2IpaPNfQ_TMn4gZko";                     // ← değiştirin
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbWR3eW91bHd0dWZoamh2ZG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTE1ODQsImV4cCI6MjA4ODg4NzU4NH0.stIDyNh8rBRJiZa8kVOoy6lh3dqOLKzghkwSoqSVbc0";                     // ← değiştirin
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
