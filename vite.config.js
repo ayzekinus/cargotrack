0148bdc1b422aa045c6f0524347a09752dc2d37d
ayzekinus <66388861+ayzekinus@users.noreply.github.com>
2026-03-12 15:24:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // 'cargotrack' yerine GitHub'daki repo adınızı yazın
-const REPO_NAME = "cargotrack";
+const REPO_NAME = "https://ayzekinus.github.io/cargotrack/";
 
 export default defineConfig({
   plugins: [react()],
